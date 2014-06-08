f479b8112120f10b13f2cfa538a3a91c0eba111a
David Thaler <davidthaler@gmail.com>
2014-06-08 13:56:18 -0700

    added prop_id rate and count features

--- R/lambda.rank.R
+++ R/lambda.rank.R
@@ -9,8 +9,23 @@ rank.split <- function(split, n.trees, score.at=NULL, seed=7){
                group="srch_id",
                metric="ndcg",
                max.rank=38)
+  if(exists('booking_bool', split$train)){
+    split$train$booking_bool <- NULL
+  }
+  if(exists('click_bool', split$train)){
+    split$train$click_bool <- NULL
+  }
+  if(exists('position', split$train)){
+    split$train$position <- NULL
+  }
+  if(exists('gross_bookings_usd', split$train)){
+    split$train$gross_bookings_usd <- NULL
+  }
+  if(exists('date_time', split$train)){
+    split$train$date_time <- NULL
+  }
   model <- gbm(rel ~ . -srch_id,
-               data=split$train[, c(1, 3:14, 16:51, 55), with=FALSE],
+               data=split$train,
                distribution=dist,
                shrinkage=0.1,
                interaction.depth=3,
--- R/util.R
+++ R/util.R
@@ -5,14 +5,16 @@ paths$data <- paste0(paths$base, 'data/')
 paths$sample <- paste0(paths$data, 'sample.csv')
 paths$train <- paste0(paths$data, 'train2.csv')
 paths$test <- paste0(paths$data, 'test2.csv')
+paths$models <- paste0(paths$base, 'models/')
+paths$submissions <- paste0(paths$base, 'submissions/')
 
 load.expedia <- function(what='sample', basic.clean=TRUE){
  x <- fread(paths[[what]])  
- if(basic.clean){
-   x[is.na(x)] <- 0
- }
  if(what %in% c('sample','train')){
    x[, rel:=4*booking_bool + click_bool]
+ }
+ if(basic.clean){
+   x[is.na(x)] <- 0
  }
  setkey(x, srch_id)
  x
@@ -27,7 +29,28 @@ train.val.split <- function(nq.train, nq.val, x=NULL, what='sample', seed=7){
   qid <- sample(qid, length(qid))
   xtr <- x[srch_id %in% qid[1:nq.train]]
   xval <- x[srch_id %in% qid[(nq.train+1):(nq.train + nq.val)]]
-  list('train'=xtr, 'val'=xval)
+  list('train'=xtr, 'val'=xval, 'qid'=qid)
+}
+
+prop.features.split <- function(split, x, smooth=100){
+  tr.qids <- unique(split$train$srch_id)
+  val.qids <- unique(split$val$srch_id)
+  f <- x[!(srch_id %in% union(tr.qids, val.qids)), 
+         list(count=.N, total.rel=sum(rel)), by=prop_id]
+  setkey(f, prop_id)
+  f[, rate:=total.rel/(count+smooth)]
+  f$total.rel <- NULL
+  split$train <- merge(split$train, f, by='prop_id', all.x=TRUE, all.y=FALSE)
+  split$val <- merge(split$val, f, by='prop_id', all.x=TRUE, all.y=FALSE)
+  split$train$count[is.na(split$train$count)] <- 0
+  split$train$rate[is.na(split$train$rate)] <- 0
+  split$val$count[is.na(split$val$count)] <- 0
+  split$val$rate[is.na(split$val$rate)] <- 0
+  o <- order(split$val$srch_id, split$val$prop_id)
+  split$val <- split$val[o]
+  o <- order(split$train$srch_id, split$train$prop_id)
+  split$train <- split$train[o]
+  split
 }
 
 
