8b980c1677b9c3d9f36ddce2e9ce97363049ebbc
David Thaler <davidthaler@gmail.com>
2014-06-08 10:49:17 -0700

    removing all metric code except ndcg3

--- R/ndcg3.R
+++ R/ndcg3.R
@@ -1,13 +1,13 @@
 require(data.table)
 
-ndcg1 <- function(rel, pred){
+ndcg1 <- function(rel, pred, k=38){
   # NB : this is designed to be used in the expression (2nd part) of data.table
   # NB2: this is not quite right...we actuall want ndcg@38
   o <- order(pred, decreasing=TRUE)
   actual <- rel[o]
   ideal <- sort(rel, decreasing=TRUE)
   dcg <- function(y) sum((2^y - 1)/log(2:(length(y)+1), base = 2))
-  ndcg <- dcg(actual)/dcg(ideal)
+  ndcg <- dcg(actual[1:k])/dcg(ideal[1:k])
 }
 
 ndcg.all <- function(val, pred){
