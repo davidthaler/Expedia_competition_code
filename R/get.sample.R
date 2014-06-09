e7fcdfd601a9fa17c246b0bcd0da8da4e8e9dd58
David Thaler <davidthaler@gmail.com>
2014-06-08 21:15:06 -0700

    removed unused code

--- R/get.sample.R
+++
@@ -1,6 +0,0 @@
-get.sample <- function(max.rows){
-  tr.col.classes <- c("numeric", "character", rep("numeric", 52))
-  train <- read.table('data/train.csv', header=T, sep=',', na.strings='NULL',
-             colClasses=tr.col.classes, nrows=max.rows)
-  list(xtrain=train[,c(1:14, 16:51)], ytrain=train[, c(1,15,52:54)])
-}
