c961075b25badeaa5da65834199aae3a7c2f0b8c
David Thaler <davidthaler@gmail.com>
2014-06-12 03:03:08 -0700

    Fixed (I think) one bug in Rfpar.

--- R/Rfpar.R
+++ R/Rfpar.R
@@ -17,10 +17,11 @@ cv.par <- function(split, maxnodes){
 }
 
 # This is how you make 1 RF in parallel
-rf.par <- function(split, maxnodes, ntree, cycles)
+rf.par <- function(split, maxnodes, ntree, cycles){
   # maxnodes, ntrees, cycles - all ints
   rf.par <- foreach(n=1:cycles, .combine=combine, .packages='randomForest') %dopar%{
                    randomForest(rel ~ . -srch_id -click_bool -position 
                                -booking_bool -gross_bookings_usd -date_time,
                                 data=split$train, ntree=ntree, maxnodes=maxnodes)
+                   }
 }
