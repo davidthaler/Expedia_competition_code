f5c350f145520dd21e4d9deb7cdbf856d922d1ff
David Thaler <davidthaler@gmail.com>
2015-06-25 17:07:02 -0700

    added doc to lambda.rank.R

--- lambda.rank.R
+++ lambda.rank.R
@@ -1,9 +1,29 @@
+# Functions in lambda.rank.R implement gradient-boosting/LambdaRank models
+# for the Expedia hotel search results ranking competition on Kaggle.
+#
+# author: David Thaler
+
 require(data.table)
 require(gbm)
 source('ndcg3.R')
 
 
 rank.split <- function(split, n.trees, score.at=NULL, depth=3, seed=7){
+  # Takes a trainin/validation split and produces a trained model and an
+  # NDCG score for that model over the validation set. 
+  # Model is trained to minimize NDCG.
+  #
+  # args:
+  #   split - a list with the training and validation sets as elements.
+  #   n.trees - # of trees to use in the GBM
+  #   score.at - A number or vector of numbers for tree counts to score at. 
+  #     Validation set is scored using every number of trees in score.at.
+  #     Default is NULL, which just uses all trees.
+  #   depth - Depth of trees in the GBM. Default is 3.
+  #   seed - integer. The random number seed. Default is 7.
+  #
+  # return:
+  #   the trained model; also prints validation score
   set.seed(seed)
   dist <- list(name="pairwise",
                group="srch_id",
@@ -28,6 +48,21 @@ rank.split <- function(split, n.trees, score.at=NULL, depth=3, seed=7){
 }
 
 train.mrr <- function(split, n.trees, score.at=NULL, depth=3, seed=7){
+  # Takes a trainin/validation split and produces a trained model and an
+  # NDCG score for that model over the validation set. 
+  # Model is trained to minimize mean reciprocal rank (MRR).
+  #
+  # args:
+  #   split - a list with the training and validation sets as elements.
+  #   n.trees - # of trees to use in the GBM
+  #   score.at - A number or vector of numbers for tree counts to score at. 
+  #     Validation set is scored using every number of trees in score.at.
+  #     Default is NULL, which just uses all trees.
+  #   depth - Depth of trees in the GBM. Default is 3.
+  #   seed - integer. The random number seed. Default is 7.
+  #
+  # return:
+  #   the trained model; also prints validation score
   set.seed(seed)
   dist <- list(name="pairwise",
                group="srch_id",
@@ -52,6 +87,21 @@ train.mrr <- function(split, n.trees, score.at=NULL, depth=3, seed=7){
 }
 
 train.beroulli <- function(split, n.trees, score.at=NULL, depth=3, seed=7){
+  # Takes a trainin/validation split and produces a trained model and an
+  # NDCG score for that model over the validation set. 
+  # Model is trained to minimize log-loss on click_bool.
+  #
+  # args:
+  #   split - a list with the training and validation sets as elements.
+  #   n.trees - # of trees to use in the GBM
+  #   score.at - A number or vector of numbers for tree counts to score at. 
+  #     Validation set is scored using every number of trees in score.at.
+  #     Default is NULL, which just uses all trees.
+  #   depth - Depth of trees in the GBM. Default is 3.
+  #   seed - integer. The random number seed. Default is 7.
+  #
+  # return:
+  #   the trained model; also prints validation score
   set.seed(seed)
   model <- gbm(click_bool ~ . -srch_id -rel -date_time
                -booking_bool -position -gross_bookings_usd,
